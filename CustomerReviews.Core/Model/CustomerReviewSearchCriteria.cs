c58d57902ac42bdf4358c6eab9a08a1ea6ad8ab8
Pavel <ppa@virtoway.com>
2019-02-22 06:10:06 -0800

    Created base test module "Customer Review"

---
+++ CustomerReviews.Core/Model/CustomerReviewSearchCriteria.cs
@@ -0,0 +1,10 @@
+﻿using VirtoCommerce.Domain.Commerce.Model.Search;
+
+namespace CustomerReviews.Core.Model
+{
+    public class CustomerReviewSearchCriteria : SearchCriteriaBase
+    {
+        public string[] ProductIds { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
